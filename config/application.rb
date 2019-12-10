a2fdd3edc04eb69f782192c1e0a70cda7c5cb971
Marcos Cannabrava <mpcannabrava@gmail.com>
2019-12-10 20:43:05 -0300

    fix banner and schema

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Saverz
        end
     end
 
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

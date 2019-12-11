b4cbb626010d62371b08239522e9a26007882717
Marcos Cannabrava <mpcannabrava@gmail.com>
2019-12-11 10:53:26 -0300

    fix un goals order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @fields = Field.all
+    @fields = Field.sorted_by_index
   end
 end
